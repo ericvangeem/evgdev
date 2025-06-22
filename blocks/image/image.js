f787bd38a13f0e318beceb9fe3e7785962902445
Eric VanGeem <eric.vangeem@herodigital.com>
2025-06-22 09:49:44 -0500

    image block.

---
+++ blocks/image/image.js
@@ -0,0 +1,18 @@
+export default async function decorate(block) {
+  // Find the picture element and caption within the existing structure
+  const picture = block.querySelector('picture');
+  const caption = block.querySelector('p:not(:has(picture))');
+  
+  if (picture) {
+    // Add classes to the existing elements for styling
+    if (caption) {
+      caption.className = 'image-caption';
+    }
+    
+    // Add a class to the picture container for styling
+    const pictureContainer = picture.closest('p');
+    if (pictureContainer) {
+      pictureContainer.className = 'image-container';
+    }
+  }
+} 
