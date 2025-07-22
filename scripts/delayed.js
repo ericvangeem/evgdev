2ed29cc2bb7e04578a05acee4c661a6010f270c8
Eric VanGeem <eric.vangeem@herodigital.com>
2025-07-22 13:54:48 -0500

    add analytics.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,15 @@
-// add delayed functionality here
+(function() {
+  // Load the GTM script asynchronously
+  var gtmScript = document.createElement('script');
+  gtmScript.async = true;
+  gtmScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-K5HMYZ7MGF';
+  document.head.appendChild(gtmScript);
+
+  // Set up the dataLayer and gtag function
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  window.gtag = gtag; // Expose gtag globally if needed
+
+  gtag('js', new Date());
+  gtag('config', 'G-K5HMYZ7MGF');
+})();
